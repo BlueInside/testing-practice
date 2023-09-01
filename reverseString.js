3e9803ad05acaded5d0937023223df83350398f4
Karol <kpulawski13@gmail.com>
2023-09-01 13:12:01 +0100

    add tests

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,9 @@
-function reverseString(string = '') {
+function reverseString(string) {
+  if (typeof string !== 'string') throw Error('Please provide a string');
+  if (string.length === 0) throw Error('String is empty');
   const result = [...string];
   return result.reverse().join('');
 }
 
-const string = 'karol';
 console.log(reverseString('karol'));
 export default reverseString;
--- reverseString.test.js
+++ reverseString.test.js
@@ -4,6 +4,16 @@ test('Return reversed string', () => {
   expect(reverseString('karol')).toMatch('lorak');
 });
 
-test('Return undefined if string is empty', () => {
-  expect(reverseString('')).toBeUndefined();
+test('Throws error if string is empty', () => {
+  expect(() => reverseString('')).toThrow();
+});
+
+test('Works with numbers as string', () => {
+  expect(reverseString('12345')).toMatch('54321');
+});
+
+test('Does not work with not string', () => {
+  expect(() => {
+    reverseString(1234);
+  }).toThrow();
 });
