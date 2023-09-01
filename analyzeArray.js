19da4d143ee0619b8b481d7419e5a6c1059ca9a5
Karol <kpulawski13@gmail.com>
2023-09-01 19:30:09 +0100

    implement tests

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,5 +1,24 @@
-function analyzeArray() {
-  return null;
+function analyzeArray(numArray) {
+  const average = calcAverageValue(numArray);
+  const min = calcMinValue(numArray);
+
+  function calcAverageValue(array) {
+    const sum = array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+    sum;
+    return parseFloat((sum / array.length).toFixed(1));
+  }
+
+  function calcMinValue(array) {
+    const min = array.reduce((accu, value) => {
+      if (value < accu) return value;
+      else return accu;
+    });
+    return min;
+  }
+  return { average, min };
 }
 
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
 export default analyzeArray;
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,1 +1,13 @@
 import analyzeArray from './analyzeArray';
+
+test('Function takes in Array of numbers and return average val', () => {
+  const testArray = [1, 8, 3, 4, 2, 6];
+  const result = analyzeArray(testArray);
+  expect(result.average).toBe(4);
+});
+
+test('Function takes in Array of numbers and return min val', () => {
+  const testArray = [1, 8, 3, 4, 2, 6];
+  const result = analyzeArray(testArray);
+  expect(result.min).toBe(1);
+});
