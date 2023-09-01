579155c1b3cd1055c31c6b0034e0c41f68d66ded
Karol <kpulawski13@gmail.com>
2023-09-01 11:52:48 +0100

    finish capitalize function and all tests

--- capitalize.js
+++ capitalize.js
@@ -1,8 +1,13 @@
 function capitalize(phrase = '') {
-  if (typeof phrase == 'string') {
-    return phrase.slice(0, 1).toUpperCase();
-  }
-  return undefined;
+  if (typeof phrase !== 'string') return undefined;
+  if (phrase.length === 0) return '';
+  console.log(phrase);
+
+  let trimmedPhrase = phrase.trim();
+  const firstNonWhitespaceIndex = phrase.indexOf(trimmedPhrase);
+  const capitalizedPart =
+    trimmedPhrase.charAt(0).toUpperCase() + trimmedPhrase.slice(1);
+  return phrase.slice(0, firstNonWhitespaceIndex) + capitalizedPart;
 }
 
 export default capitalize;
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,13 +1,32 @@
 import capitalize from './capitalize';
 
 test('capitalize string longer than 0 chars', () => {
-  expect(capitalize('hello')).toMatch('H');
+  expect(capitalize('hello')).toMatch('Hello');
 });
 
 test('Return empty string if no argument provided', () => {
   expect(capitalize()).toMatch('');
 });
 
-test('returns undefined if argument is not a string', () => {
+test('Returns undefined if argument is not a string', () => {
   expect(capitalize(12304)).toBeUndefined();
+});
+
+test('Return empty string for an empty input string', () => {
+  expect(capitalize('')).toMatch('');
+});
+
+test('Does not change all-uppercase string', () => {
+  expect(capitalize('HELLO')).toMatch('HELLO');
+});
+
+test('handles no letters characters without change', () => {
+  expect(capitalize('123ABC')).toMatch('123ABC');
+});
+
+test('Handles leading whitespace', () => {
+  expect(capitalize('   hello')).toMatch('   H');
+});
+test('Handles non-ASCII characters', () => {
+  expect(capitalize('über')).toMatch('Ü');
 });
